d83b21739733ef508a218d9f1f95898b299ef688
ChiaWeiNam <0113487@kdu-online.com>
2016-09-30 14:19:13 +0800

    Camera Follow Player

---
+++ Operation MonsterRush/Assets/CameraScripts.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScripts : MonoBehaviour 
+{
+	public float distanceAway;
+	public float distanceUp;
+	public float smooth;
+	private Transform follow;
+	private Vector3 targetPosition;
+
+	void Start()
+	{
+		follow = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+	void LateUpdate()
+	{
+		targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
+
+		transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+
+		transform.LookAt(follow);
+	}
+}
+
