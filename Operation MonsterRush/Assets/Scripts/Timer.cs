873a8fc9cfbb107bf5138c47a729219c813a5809
kurusaki234 <0111338@kdu-online.com>
2016-10-13 01:26:23 +0800

    Timer
    Last minute update gomenasai

---
+++ Operation MonsterRush/Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour 
+{
+	float timer, realTime;
+	int minutes, seconds, fraction;
+
+	public Text timerText;
+
+	void Awake()
+	{
+		timer = 0.0f;
+		realTime = Time.time;
+
+		timerText = this.transform.GetComponent <Text>();
+	}
+
+	void FixedUpdate()
+	{
+		timer = Time.time - realTime;
+
+		minutes = (int) timer / 60;
+		seconds = (int) timer % 60;
+
+		timerText.text = string.Format ( "{0:00} : {1:00}", minutes, seconds);
+	}
+}
