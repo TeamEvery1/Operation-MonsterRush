98ab17a3a33396f09ccd60beaf5898f77b1626b5
ChiaWeiNam <0113487@kdu-online.com>
2016-12-01 18:29:38 +0800

    Rock Damage Player and little push back

---
+++ Operation MonsterRush/Assets/Scripts/Enemy/LusoRockScirpts.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LusoRockScirpts : MonoBehaviour 
+{
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			other.transform.position -= new Vector3(0.0f, -50.0f, 50.0f) * Time.deltaTime * 1.0f;
+			other.transform.GetComponent<Player.Controller>().health -= 1;
+			Destroy(this.gameObject);
+		}
+	}
+}
